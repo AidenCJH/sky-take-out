158820db2d3971f07e774c4df5593645baec579e
Aiden <chen_jinhao@foxmail.com>
2024-02-14 15:00:50 +0800

    开发订单状态定时处理的功能。2024.2.14

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -2,20 +2,42 @@ package com.sky.mapper;
 
 import com.sky.entity.Orders;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Mapper
 public interface OrderMapper {
     /**
      * 插入订单数据
+     *
      * @param orders
      */
     void insert(Orders orders);
 
     /**
      * 设置订单为已支付（根据订单号）
+     *
      * @param orders
      */
     @Update("update orders set pay_method=#{payMethod},status=#{status},pay_status=#{payStatus},checkout_time=#{checkoutTime} where number=#{number}")
     void setPaid(Orders orders);
+
+    /**
+     * 根据状态和下单时间查询订单
+     *
+     * @param status
+     * @param orderTime
+     * @return
+     */
+    @Select("select * from orders where status=#{status} and order_time<#{orderTime}")
+    List<Orders> getByStatusAndOrderTimeLT(Integer status, LocalDateTime orderTime);
+
+    /**
+     * 更新订单
+     * @param orders
+     */
+    void update(Orders orders);
 }
